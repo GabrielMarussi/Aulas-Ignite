0362a37c27e99ef8fe01f154d7e5fd5e1c1ba954
GabrielMarussi <gabrielmesquitacwb@gmail.com>
2022-05-06 17:34:27 -0300

    Implementando FaunaDB
    Configurando FaunaDB no projeto e Botão SignIn funcionando com o FaunaDB.

--- aula-03/src/components/Header/index.tsx
+++ aula-03/src/components/Header/index.tsx
@@ -3,11 +3,12 @@ import Img from 'next/image'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
+
 export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <Img src="/images/logo.svg" alt="ig.news" />
+                <img src="/images/logo.svg" alt="ig.news" />
                 
                 <nav>
                     <a className={styles.active}>Home</a>
--- aula-03/src/components/SignInButton/index.tsx
+++ aula-03/src/components/SignInButton/index.tsx
@@ -1,18 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
-    
+    const {data: session} = useSession()
+
     const RenderButtonLogged = (
         <button 
-            className={styles.signInButton}
             type="button"
+            className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            Gabriel Marussi
+            {session?.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     )
@@ -21,6 +23,7 @@ export function SignInButton(){
         <button 
             className={styles.signInButton}
             type="button"
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sign in with Github 
@@ -28,6 +31,6 @@ export function SignInButton(){
 
     )
 
-    return isUserLoggedIn ? RenderButtonLogged : RenderButtonNotLogged
+    return session ? RenderButtonLogged : RenderButtonNotLogged
 
 }
