7504020b59aac81d0c7d38c9337fe5908f9a17bc
Gabriel Marussi <gabrielmesquitacwb@gmail.com>
2022-05-19 18:02:32 -0300

    Configuração dos Posts
    Foi configurado e ligado a pagina de Posts, usando o FaunaDB como banco de dados para verificar se foi inscrito e utilizar os dados, o Prismic para a criação de cada post.

--- aula-03/src/components/Header/index.tsx
+++ aula-03/src/components/Header/index.tsx
@@ -1,19 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
-import Img from 'next/image'
 
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
+import { ActiveLink } from '../ActiveLink'
+import { useRouter } from 'next/router'
+
+
+
 
 export function Header(){
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href='/'>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href='/posts'>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 
                 <SignInButton />
--- aula-03/src/components/SubscribeButton/index.tsx
+++ aula-03/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './style.module.scss'
@@ -9,25 +10,30 @@ interface SubsCribeButtonProps{
 
 export function SubscribeButton({priceId}: SubsCribeButtonProps){
     const {data: session} = useSession();
-
+    const router = useRouter();
     
     async function handleSubscribe(){
         if (!session){
-            signIn('github')
+            signIn('github');
+            return;
+        }
+
+        if (session.activeSubscription){
+            router.push('/posts');
             return;
         }
 
         try {
-            const response = await api.post('/subscribe')
+            const response = await api.post('/subscribe');
 
             const { sessionId } = response.data;
 
-            const stripe = await getStripeJs()
+            const stripe = await getStripeJs();
 
-            await stripe.redirectToCheckout({sessionId})
+            await stripe.redirectToCheckout({sessionId});
 
         } catch (err){
-            alert(err.message)
+            alert(err.message);
         }
 
     }
